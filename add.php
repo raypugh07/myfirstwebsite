e8b0f96aa867839a40797414ebda1b0700e5f0a0
Your Name <you@example.com>
2019-11-18 18:47:48 -0600

    user session,can add to user lists,connects with db,add.php

---
+++ add.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    if($_SESSION['user']){
+    }
+    else{ 
+       header("location:index.php");
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+       $details = $_POST['details'];
+       $time = strftime("%X"); //time
+       $date = strftime("%B %d, %Y"); //date
+       $decision = "no";
+   
+       $mysqli = mysqli_connect("localhost", "root", ""); //connects to server
+       mysqli_select_db($mysqli,"first_db"); //connects to database
+       $order = "INSERT INTO list(details, date_posted, time_posted, public) VALUES ('$details','$date','$time','$decision')"; //SQL query
+       $result = mysqli_query($mysqli,$order);
+       header("location:home.php");
+       /* foreach($_POST["public"] in $each_check) //gets the data from the checkbox
+       {
+          if($each_check != null){ //checks if checkbox is checked
+             $decision = "yes"; // sets the value
+          }
+       }
+
+       mysqli_query("INSERT INTO list(details, date_posted, time_posted, public) VALUES ('$details','$date','$time','$decision')"); //SQL query
+       header("location:home.php");
+    }
+    else
+    {
+       header("location:home.php");
+    }*/
+}
+?>
